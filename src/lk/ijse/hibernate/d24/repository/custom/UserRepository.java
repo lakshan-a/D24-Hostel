607764c9100795039160ee792703b2f08c14ccb7
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-09-02 23:05:58 +0530

    add user

--- src/lk/ijse/hibernate/d24/repository/custom/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package lk.ijse.hibernate.d24.repository.custom;
-
-import lk.ijse.hibernate.d24.entity.UserEntity;
-import lk.ijse.hibernate.d24.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserEntity> {
-    public String getPassword(String username);
-}
