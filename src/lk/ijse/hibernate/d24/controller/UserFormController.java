ba848ce17c7ab1a873c3de910f5f503d3cd741bc
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-09-02 23:07:40 +0530

    add user

--- src/lk/ijse/hibernate/d24/controller/UserFormController.java
+++ src/lk/ijse/hibernate/d24/controller/UserFormController.java
@@ -1,4 +1,31 @@
 package lk.ijse.hibernate.d24.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import java.io.IOException;
+
 public class UserFormController {
+    public AnchorPane pane;
+    public Text txtName;
+    public Text txtUsername;
+    public Text txtPassword;
+    public Text txtTel;
+    public TextField txtNewUsername;
+    public TextField txtNewPassword;
+    public TextField txtConfirmPassword;
+    public TextField txtCurrentUsername;
+
+
+    public void initialize(){
+
+    }
+
+    public void btnSaveChangesOnAction(ActionEvent actionEvent) {
+
+    }
 }
