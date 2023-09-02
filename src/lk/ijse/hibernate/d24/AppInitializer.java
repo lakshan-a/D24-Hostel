3bc50bb4f76516f791fa5101e9dbfa1e4ec4d0e4
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-09-02 19:59:39 +0530

    add project

---
+++ src/lk/ijse/hibernate/d24/AppInitializer.java
@@ -0,0 +1,19 @@
+package lk.ijse.hibernate.d24;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class AppInitializer{
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+
+}
