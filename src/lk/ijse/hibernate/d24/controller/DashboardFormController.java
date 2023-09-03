ac860b82ae8deda38e888b4112931e88a5ec54de
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-09-03 11:24:34 +0530

    create dashboard

---
+++ src/lk/ijse/hibernate/d24/controller/DashboardFormController.java
@@ -0,0 +1,45 @@
+package lk.ijse.hibernate.d24.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+//import lk.ijse.hibernate.d24.repository.custom.StudentRepository;
+//import lk.ijse.hibernate.d24.repository.custom.impl.StudentRepositoryImpl;
+import lk.ijse.hibernate.d24.util.Navigation;
+import lk.ijse.hibernate.d24.util.Routes;
+
+import java.io.IOException;
+
+public class DashboardFormController {
+    public AnchorPane primaryPane;
+    public AnchorPane secondaryPane;
+
+    public void initialize(){
+
+    }
+
+    public void btnReservationOnAction(ActionEvent actionEvent) throws IOException {
+        Navigation.navigate(Routes.RESERVATIONFORM, secondaryPane);
+    }
+
+    public void btnStudentOnAction(ActionEvent actionEvent) throws IOException {
+        Navigation.navigate(Routes.STUDENTFORM, secondaryPane);
+    }
+
+    public void btnRoomOnAction(ActionEvent actionEvent) throws IOException {
+        Navigation.navigate(Routes.ROOMFORM, secondaryPane);
+    }
+
+    public void btnStudentRegOnAction(ActionEvent actionEvent) throws IOException {
+        Navigation.navigate(Routes.STUDENTFORM, secondaryPane);
+    }
+
+    public void btnUserOnAction(ActionEvent actionEvent) throws IOException {
+        Navigation.navigate(Routes.USER, secondaryPane);
+    }
+
+    public void btnLogOutOnAction(ActionEvent actionEvent) throws IOException {
+        Navigation.navigate(Routes.LOGIN, primaryPane);
+    }
+}
