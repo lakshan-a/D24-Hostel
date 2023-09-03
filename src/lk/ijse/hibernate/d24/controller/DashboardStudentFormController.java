c56c948f9379ac9626429ab408e0578657973fa3
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-09-03 23:34:06 +0530

    complite ui

---
+++ src/lk/ijse/hibernate/d24/controller/DashboardStudentFormController.java
@@ -0,0 +1,86 @@
+package lk.ijse.hibernate.d24.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class DashboardStudentFormController {
+
+    @FXML
+    private ComboBox<?> cmbGender;
+
+    @FXML
+    private ComboBox<?> cmbGenderSearch;
+
+    @FXML
+    private TextField txtFldAddressReg;
+
+    @FXML
+    private TextField txtFldAddressSearch;
+
+    @FXML
+    private TextField txtFldDobReg;
+
+    @FXML
+    private TextField txtFldNameReg;
+
+    @FXML
+    private TextField txtFldNameSearch;
+
+    @FXML
+    private TextField txtFldSDobSearch;
+
+    @FXML
+    private TextField txtFldStdentIdSearch;
+
+    @FXML
+    private TextField txtFldStudentIdReg;
+
+    @FXML
+    private TextField txtFldTelReg;
+
+    @FXML
+    private TextField txtFldTelSearch;
+
+    @FXML
+    private Text txtInvalidDateError;
+
+    @FXML
+    private Text txtInvalidNameError;
+
+    @FXML
+    private Text txtInvalidTelError;
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAcion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegisterOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAcion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnViewAllOnAcion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtFldSearchIdOnAction(ActionEvent event) {
+
+    }
+
+}
