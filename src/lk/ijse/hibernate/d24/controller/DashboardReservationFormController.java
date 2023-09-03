3e491a1b49ccb7955dd27246a50fed174b422b3b
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-09-03 20:35:44 +0530

    create reservation

---
+++ src/lk/ijse/hibernate/d24/controller/DashboardReservationFormController.java
@@ -0,0 +1,88 @@
+package lk.ijse.hibernate.d24.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class DashboardReservationFormController {
+
+    @FXML
+    private ComboBox<?> cmbStatusReg;
+
+    @FXML
+    private ComboBox<?> cmbStatusSearch;
+
+    @FXML
+    private Text txtAcFoodRooms;
+
+    @FXML
+    private Text txtAcRooms;
+
+    @FXML
+    private TextField txtFldDateReg;
+
+    @FXML
+    private TextField txtFldDateSearch;
+
+    @FXML
+    private TextField txtFldReservationIdSearch;
+
+    @FXML
+    private TextField txtFldRoomTypeIdReg;
+
+    @FXML
+    private TextField txtFldRoomTypeIdSearch;
+
+    @FXML
+    private TextField txtFldStudentIdReg;
+
+    @FXML
+    private TextField txtFldStudentIdSearch;
+
+    @FXML
+    private Text txtNonAcFoodRooms;
+
+    @FXML
+    private Text txtNonAcRooms;
+
+    @FXML
+    private TextField txtReservationIdReg;
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAcion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNotPayOnAcion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegisterOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAcion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnViewAllOnAcion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtFldReservationIdOnAction(ActionEvent event) {
+
+    }
+
+}
