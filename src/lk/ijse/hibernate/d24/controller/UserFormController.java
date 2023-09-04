239fb8114f820717e6b02ba3e38f6e86219b1744
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-09-04 12:23:31 +0530

    create User

--- src/lk/ijse/hibernate/d24/controller/UserFormController.java
+++ src/lk/ijse/hibernate/d24/controller/UserFormController.java
@@ -7,6 +7,15 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
+import lk.ijse.hibernate.d24.bo.BOFactory;
+import lk.ijse.hibernate.d24.bo.custom.UserBO;
+import lk.ijse.hibernate.d24.dto.UserDTO;
+import lk.ijse.hibernate.d24.entity.UserEntity;
+import lk.ijse.hibernate.d24.repository.custom.UserRepository;
+import lk.ijse.hibernate.d24.repository.custom.impl.UserRepostoryImpl;
+import lk.ijse.hibernate.d24.util.Navigation;
+import lk.ijse.hibernate.d24.util.Routes;
+
 import java.io.IOException;
 
 public class UserFormController {
@@ -20,12 +29,28 @@ public class UserFormController {
     public TextField txtConfirmPassword;
     public TextField txtCurrentUsername;
 
+    UserBO userBO = (UserBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.USER);
 
     public void initialize(){
+        UserDTO user = userBO.searchUser(LoginFormController.username);
 
+        txtName.setText(user.getName());
+        txtUsername.setText(user.getUserName());
+        txtPassword.setText(user.getPassword());
+        txtTel.setText(user.getTel());
     }
 
     public void btnSaveChangesOnAction(ActionEvent actionEvent) {
-
+        if(txtUsername.getText() .equals(txtCurrentUsername.getText())){
+            if(txtNewPassword.getText() .equals(txtConfirmPassword.getText())){
+                userBO.updateUser(new UserDTO(txtCurrentUsername.getText(), txtConfirmPassword.getText(), txtName.getText(), txtTel.getText()));
+                new Alert(Alert.AlertType.CONFIRMATION,"Username and Password Updated Successfully").show();
+                initialize();
+            }else{
+                new Alert(Alert.AlertType.WARNING,"Passwords are not match").show();
+            }
+        }else{
+            new Alert(Alert.AlertType.WARNING,"Invalid Username").show();
+        }
     }
 }
