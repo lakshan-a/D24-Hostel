3962ac2ef9895279e49c46024e450b9ca6ad1645
Lakshan-a <lakshanrashmika16392@gmail.com>
2023-09-04 12:00:32 +0530

    create DashboradRoom

--- src/lk/ijse/hibernate/d24/controller/DashboardRoomFormController.java
+++ src/lk/ijse/hibernate/d24/controller/DashboardRoomFormController.java
@@ -1,76 +1,146 @@
 package lk.ijse.hibernate.d24.controller;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import lk.ijse.hibernate.d24.bo.BOFactory;
+import lk.ijse.hibernate.d24.bo.custom.RoomBO;
+import lk.ijse.hibernate.d24.dto.RoomDTO;
+import lk.ijse.hibernate.d24.util.Navigation;
+import lk.ijse.hibernate.d24.util.Routes;
+
+import java.io.IOException;
 
 public class DashboardRoomFormController {
+    public TextField txtFldRoomTypeIdReg;
+    public TextField txtFldRoomTypeReg;
+    public TextField txtFldQtyReg;
+    public TextField txtFldKeyMoneyReg;
+    public TextField txtFldTypeSearch;
+    public TextField txtFldQtySearch;
+    public TextField txtFldSKeyMoneySearch;
+    public TextField txtFldRoomTypeIdSearch;
+    public AnchorPane secondaryPane;
+    public Text txtNonAcRooms;
+    public Text txtNonAcFoodRooms;
+    public Text txtAcRooms;
+    public Text txtAcFoodRooms;
 
-    @FXML
-    private Text txtAcFoodRooms;
+    public void initialize(){
+        clearReg();
+        clearSearch();
 
-    @FXML
-    private Text txtAcRooms;
+        txtFldRoomTypeIdReg.setText(roomBO.nextRoomId());
 
-    @FXML
-    private TextField txtFldKeyMoneyReg;
+        try {
+            txtAcRooms.setText(String.valueOf(roomBO.getRoomCount("RM-7896")));
+        }catch (Exception e){
+            txtAcRooms.setText("0");
+        }
 
-    @FXML
-    private TextField txtFldQtyReg;
+        try {
+            txtAcFoodRooms.setText(String.valueOf(roomBO.getRoomCount("RM-0093")));
+        }catch (Exception e){
+            txtAcFoodRooms.setText("0");
+        }
 
-    @FXML
-    private TextField txtFldQtySearch;
+        try {
+            txtNonAcRooms.setText(String.valueOf(roomBO.getRoomCount("RM-1324")));
+        }catch (Exception e){
+            txtNonAcRooms.setText("0");
+        }
 
-    @FXML
-    private TextField txtFldRoomTypeIdReg;
-
-    @FXML
-    private TextField txtFldRoomTypeIdSearch;
-
-    @FXML
-    private TextField txtFldRoomTypeReg;
-
-    @FXML
-    private TextField txtFldSKeyMoneySearch;
-
-    @FXML
-    private TextField txtFldTypeSearch;
-
-    @FXML
-    private Text txtNonAcFoodRooms;
-
-    @FXML
-    private Text txtNonAcRooms;
-
-    @FXML
-    void btnClearOnAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnDeleteOnAcion(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnRegisterOnAction(ActionEvent event) {
+        try {
+            txtNonAcFoodRooms.setText(String.valueOf(roomBO.getRoomCount("RM-5467")));
+        }catch (Exception e){
+            txtNonAcFoodRooms.setText("0");
+        }
 
     }
 
-    @FXML
-    void btnUpdateOnAcion(ActionEvent event) {
+    private void clearReg(){
+        txtFldKeyMoneyReg.clear();
+        txtFldQtyReg.clear();
+        txtFldRoomTypeIdReg.clear();
+        txtFldRoomTypeReg.clear();
+    }
+
+    private void clearSearch(){
+        txtFldQtySearch.clear();
+        txtFldTypeSearch.clear();
+        txtFldRoomTypeIdSearch.clear();
+        txtFldSKeyMoneySearch.clear();
+    }
+
+    RoomBO roomBO = (RoomBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.ROOM);
+
+    public void btnRegisterOnAction(ActionEvent actionEvent) {
+        roomBO.saveRoom(new RoomDTO(
+                txtFldRoomTypeIdReg.getText(),
+                txtFldKeyMoneyReg.getText(),
+                Integer.valueOf(txtFldQtyReg.getText()),
+                txtFldRoomTypeReg.getText()
+        ));
+
+        new Alert(Alert.AlertType.CONFIRMATION,"Room Added Successfully").show();
+        clearReg();
+
+        initialize();
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+        clearReg();
+        clearSearch();
+    }
+
+    public void txtFldRoomTypeIdOnAction(ActionEvent actionEvent) {
+
+        try {
+            RoomDTO roomDTO = roomBO.searchRoom(txtFldRoomTypeIdSearch.getText());
+
+            txtFldRoomTypeIdSearch.setText(roomDTO.getRoomTypeID());
+            txtFldTypeSearch.setText(roomDTO.getType());
+            txtFldSKeyMoneySearch.setText(roomDTO.getKeyMoney());
+            txtFldQtySearch.setText(String.valueOf(roomDTO.getQty()));
+
+        }catch (Exception e){
+            new Alert(Alert.AlertType.WARNING,"Room not Found").show();
+            clearSearch();
+        }
+
 
     }
 
-    @FXML
-    void btnViewAllOnAcion(ActionEvent event) {
+    public void btnUpdateOnAcion(ActionEvent actionEvent) {
+        roomBO.updateRoom(new RoomDTO(
+                txtFldRoomTypeIdSearch.getText(),
+                txtFldSKeyMoneySearch.getText(),
+                Integer.valueOf(txtFldQtySearch.getText()),
+                txtFldRoomTypeIdSearch.getText()
+        ));
 
+        new Alert(Alert.AlertType.CONFIRMATION,"Room Updated Successfully").show();
+        clearSearch();
+
+        initialize();
     }
 
-    @FXML
-    void txtFldRoomTypeIdOnAction(ActionEvent event) {
+    public void btnDeleteOnAcion(ActionEvent actionEvent) {
+        roomBO.deleteRoom(new RoomDTO(
+                txtFldRoomTypeIdSearch.getText(),
+                txtFldSKeyMoneySearch.getText(),
+                Integer.valueOf(txtFldQtySearch.getText()),
+                txtFldRoomTypeIdSearch.getText()
+        ));
+        new Alert(Alert.AlertType.CONFIRMATION,"Room Deleted Successfully").show();
+        clearSearch();
 
+        initialize();
     }
 
+    public void btnViewAllOnAcion(ActionEvent actionEvent) throws IOException {
+        Navigation.navigate(Routes.VIEWALLROOMS, secondaryPane);
+    }
 }
